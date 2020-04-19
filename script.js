cdcb358d72cbdd45606f6289d99b31b5ba92fca2
Alex <alxdr2@gmail.com>
2020-04-20 00:19:25 +0200

    Это наш первый коммит

---
+++ script.js
@@ -0,0 +1,26 @@
+// 'use strict'
+
+// var leftBorderWidth = 1;
+// let second = 2;
+// const pi = 3.14;
+
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// null;
+// undefined;
+// var obj = {};
+
+// console.log(4/0);
+
+// let person = {
+//     name: "john",
+//     age: "20",
+//     isMarried: "false"
+// };
+// console.log(person.name);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp']
+// console.log(arr[21]);
+console.log("https://vk.com/catalog/"+5);
