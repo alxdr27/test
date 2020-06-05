74403f27acaf4982ed9ef834c5daa49473eceff6
Alex <alxdr2@gmail.com>
2020-06-05 16:55:46 +0200

    Cделал задания второго урока

--- lesson01/javascript.js
+++ lesson01/javascript.js
@@ -1,8 +1,18 @@
-var money = 0;
-var income;
-var addExpenses;
-var deposit;
-var mission;
-var period;
+var money = 1200;
+var income = "шабашки";
+var addExpenses = "Свет, Газ, Вода, Интернет, Телефон";
+var deposit = true;
+var mission = 10000;
+var period = 8;
 
-alert("Денег на счету "+money+" рублей")
+// alert("Денег на счету "+money+" рублей")
+console.log(typeof(money))
+console.log(typeof(income))
+console.log(typeof(deposit))
+console.log(addExpenses.length)
+console.log("Период равен "+period+" месяцев."+" Цель заработать "+mission+" евро")
+console.log(addExpenses.toLowerCase())
+console.log(addExpenses.split(', '))
+
+var budgetDay = money/30
+console.log(budgetDay)
