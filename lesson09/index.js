20b32b70c0c562aa44bb05cafda68c9909a0c913
Alex <alxdr2@gmail.com>
2020-06-17 20:18:23 +0200

    fix exercice e and f

--- lesson09/index.js
+++ lesson09/index.js
@@ -12,5 +12,30 @@ let checkbox = document.querySelectorAll("[type='checkbox']");
 let addIncomeItem = document.querySelectorAll(".additional_income-item")
 
 //Каждый элемент в правой части программы через класс, которые имеют в имени класса "-value", начиная с class="budget_day-value" и заканчивая class="target_month-value">
-let value = document.querySelectorAll("*[class]")
-console.log(value)
+let budgetMonthValue = document.querySelector(".budget_month-value");
+let budgetDayValue = document.querySelector(".budget_day-value")
+let expensesMonthValue = document.querySelector('.expenses_month-value')
+let additionalIncomeValue = document.querySelector(".additional_income-value")
+let additionalExpensesValue = document.querySelector(".additional_expenses-value")
+let incomePeriodValue = document.querySelector(".income_period-value")
+let targetMonthValue = document.querySelector(".target_month-value")
+
+
+
+//Оставшиеся поля через querySelector каждый в отдельную переменную:
+//поля ввода (input) с левой стороны и не забудьте про range.
+
+
+let salaryAmount = document.querySelector(".salary-amount")
+let incomeTitle = document.querySelector(".income-items>.income-title")
+let incomeAmount = document.querySelector(".income-amount")
+let additionalIncomeItem = document.querySelector(".additional_income-item")
+let expensesTitle = document.querySelector(".expenses-items>.expenses-title")
+let expensesAmount = document.querySelector(".expenses-amount")
+let additionalExpensesItem = document.querySelector(".additional_expenses-item")
+let depositCheck = document.querySelector("#deposit-check")
+let depositAmount = document.querySelector(".deposit-amount")
+let depositPercent = document.querySelector(".deposit-percent")
+let targetAmount = document.querySelector(".target-amount")
+let range = document.querySelector("[type='range']")
+console.log( range )
